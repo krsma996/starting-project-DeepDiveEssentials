d6cecd097c2a7dc30914e8c4abe234b8f8a4c609
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2025-05-21 14:16:10 +0200

    vezba
    vezba

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -35,6 +35,7 @@ export default function Examples() {
   return (
     <Section title={"Examples"} id="examples">
       <Tabs
+        ButtonsContainer="menu"
         buttons={
           <>
             <TabButton
@@ -66,7 +67,6 @@ export default function Examples() {
       >
         {tabContent}
       </Tabs>
-      <menu></menu>
     </Section>
   );
 }
