1a25447b61dc64369e21c0c021a41ca7208d51ac
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2025-05-21 14:23:09 +0200

    vezba kraj !
    vezba kraj!

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -35,7 +35,6 @@ export default function Examples() {
   return (
     <Section title={"Examples"} id="examples">
       <Tabs
-        ButtonsContainer="menu"
         buttons={
           <>
             <TabButton
